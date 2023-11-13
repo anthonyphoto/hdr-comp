ebedb519c9ffaf8b353382295f451bc39ce2aebe
Anthony Kim <okim01@nuix.com>
2023-11-13 13:23:30 -0500

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+const addListener = () => {
+
+}
+
+
+const main = () => {
+    IS_LANDSCAPE = isLandscape();
+    document.documentElement.style.fontSize = IS_LANDSCAPE ? '20px' : '16px';
+
+    const screenWidth = getWidth();
+    if (screenWidth > 2000) {
+        document.documentElement.style.setProperty('--max-width', '1400px');
+        document.documentElement.style.setProperty('--portrait-width', '900px');
+    }
+
+    const param = window.location.search.split(/=/);
+    const id = param.length === 2 ? param[1] : '';
+    // handleEvent();
+    // renderLanding(true);  
+    // if (MENU_NAMES.every(name => id !== name)) {
+    //   document.title = 'Anthony Photography';
+    //   history.pushState({ page: 'home' }, '', '');
+    //   renderLanding();
+    // } else {
+    // }
+
+    // const addImageLoadEvent = addClassLoadEvent();
+    // loadAnimeFrames(addImageLoadEvent); // initially load the frame images
+}
+
+main();
+
+
